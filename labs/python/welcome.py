363de0e1979d5f7c4bafd5a1687e592be7122e83
sumans-19 <sumanshanthakumar@gmail.com>
2025-11-29 14:08:02 +0530

    testing3

--- labs/python/welcome.py
+++ labs/python/welcome.py
@@ -35,4 +35,4 @@ def hacker_boot():
 
 if __name__ == "__main__":
     hacker_boot()
-    print("Hi ... Welcome to your Zerosetup Python Lab!")
+    print("..... Welcome to your Zerosetup Python Lab!")
